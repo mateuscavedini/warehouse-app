942e7c980ee340b16ebe6bc3c3a19cacdaed9882
Mateus Cavedini Pereira <mateuscavedini@gmail.com>
2023-10-19 02:55:54 -0300

    Usuário cadastra novo galpão

--- app/controllers/warehouses_controller.rb
+++ app/controllers/warehouses_controller.rb
@@ -2,4 +2,16 @@ class WarehousesController < ApplicationController
   def show
     @warehouse = Warehouse.find(params[:id])
   end
+
+  def new
+    @warehouse = Warehouse.new
+  end
+
+  def create
+    warehouse_params = params.require(:warehouse).permit(:name, :code, :city, :description, :address, :cep, :area)
+
+    Warehouse.create(warehouse_params)
+
+    redirect_to root_path, notice: 'Galpão cadastrado com sucesso!'
+  end
 end
