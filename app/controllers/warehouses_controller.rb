6cff03ed666594db180fddefb6d396a2ff1d2a28
Mateus Cavedini Pereira <mateuscavedini@gmail.com>
2023-10-20 03:16:01 -0300

    Remoção de repetições de código

--- app/controllers/warehouses_controller.rb
+++ app/controllers/warehouses_controller.rb
@@ -1,15 +1,13 @@
 class WarehousesController < ApplicationController
-  def show
-    @warehouse = Warehouse.find(params[:id])
-  end
+  before_action :set_warehouse, only: [:show, :edit, :update]
+
+  def show; end
 
   def new
     @warehouse = Warehouse.new
   end
 
   def create
-    warehouse_params = params.require(:warehouse).permit(:name, :code, :city, :description, :address, :cep, :area)
-
     @warehouse = Warehouse.new(warehouse_params)
 
     if @warehouse.save
@@ -20,20 +18,24 @@ class WarehousesController < ApplicationController
     end
   end
 
-  def edit
-    @warehouse = Warehouse.find(params[:id])
-  end
+  def edit; end
 
   def update
-    @warehouse = Warehouse.find(params[:id])
-    warehouse_params = params.require(:warehouse).permit(:name, :code, :city, :description, :address, :cep, :area)
-
     if @warehouse.update(warehouse_params)
       redirect_to warehouse_path(@warehouse.id), notice: 'Galpão atualizado com sucesso!'
     else
       flash.now[:notice] = 'Não foi possível atualizar o galpão.'
       render :edit
     end
+  end
 
+  private
+
+  def set_warehouse
+    @warehouse = Warehouse.find(params[:id])
+  end
+
+  def warehouse_params
+    params.require(:warehouse).permit(:name, :code, :city, :description, :address, :cep, :area)
   end
 end
