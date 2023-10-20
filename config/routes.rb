2aa9a24d93e3c4f8d35ac4ad376087c4b38c3534
Mateus Cavedini Pereira <mateuscavedini@gmail.com>
2023-10-20 19:51:46 -0300

    Usuário vê fornecedores

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root to: 'home#index'
   resources :warehouses, only: [:show, :new, :create, :edit, :update, :destroy]
+  resources :suppliers, only: [:index]
 
   get "up" => "rails/health#show", as: :rails_health_check
 end
