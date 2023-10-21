4676c4b6303265ae4a39097ddecde61685b82dce
Mateus Cavedini Pereira <mateuscavedini@gmail.com>
2023-10-21 15:40:00 -0300

    Usuário vê modelos de produtos

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root to: 'home#index'
   resources :warehouses, only: [:show, :new, :create, :edit, :update, :destroy]
   resources :suppliers, only: [:index, :show, :new, :create, :edit, :update]
+  resources :product_models, only: [:index]
 
   get "up" => "rails/health#show", as: :rails_health_check
 end
