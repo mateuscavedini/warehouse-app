60915ab928eba39c7cfd26bcc6399b795d54d425
Mateus Cavedini Pereira <mateuscavedini@gmail.com>
2023-10-23 20:09:36 -0300

    Usuário cadastra e vê detalhes de um pedido

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,37 @@
+class OrdersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_order, only: [:show]
+
+  def show; end
+
+  def new
+    @order = Order.new
+    @warehouses = Warehouse.all
+    @suppliers = Supplier.all
+  end
+
+  def create
+    @order = Order.new(order_params)
+    @order.user = current_user
+
+    if @order.save
+      redirect_to @order, notice: 'Pedido registrado com sucesso!'
+    else
+      @warehouses = Warehouse.all
+      @suppliers = Supplier.all
+
+      flash.now[:alert] = 'Pedido não registrado.'
+      render :new
+    end
+  end
+
+  private
+
+  def set_order
+    @order = Order.find(params[:id])
+  end
+
+  def order_params
+    params.require(:order).permit(:warehouse_id, :supplier_id, :estimated_delivery_date)
+  end
+end
---
+++ spec/system/orders/user_register_order_spec.rb
@@ -0,0 +1,34 @@
+require 'rails_helper'
+
+describe 'Usuário cadastra um pedido' do
+  it 'e deve estar autenticado' do
+    visit root_path
+    click_on 'Registrar Pedido'
+
+    expect(current_path).to eq new_user_session_path
+  end
+  
+  it 'com sucesso' do
+    user = User.create!(name: 'Sergio', email: 'sergio@email.com', password: '123456')
+    Warehouse.create!(name: 'Maceio', code: 'MCZ', city: 'Maceio', area: 50_000, address: 'Av Atlantica, 50', cep: '80000-000', description: 'Perto do Aeroporto')
+    warehouse = Warehouse.create!(name: 'Aeroporto SP', code: 'GRU', city: 'Guarulhos', area: 100_000, address: 'Avenida do Aeroporto, 1000', cep: '15000-000', description: 'Galpão destinado para cargas internacionais')
+    Supplier.create!(corporate_name: 'Spark Industries Brasil LTDA', brand_name: 'Spark', registration_number: '36216140000158', full_address: 'Torre da Indústria, 1', city: 'Teresina', state: 'PI', email: 'vendas@spark.com')
+    supplier = Supplier.create!(corporate_name: 'ACME LTDA', brand_name: 'ACME', registration_number: '47176140000189', full_address: 'Av das Palmas, 100', city: 'Bauru', state: 'SP', email: 'contato@acme.com')
+
+    visit root_path
+    login_as(user)
+    click_on 'Registrar Pedido'
+    select warehouse.name, from: 'Galpão Destino'
+    select supplier.corporate_name, from: 'Fornecedor'
+    fill_in 'Data Prevista de Entrega', with: '20/12/2023'
+    click_on 'Gravar'
+
+    expect(page).to have_content 'Pedido registrado com sucesso!'
+    expect(page).to have_content 'Galpão Destino: Aeroporto SP'
+    expect(page).to have_content 'Fornecedor: ACME LTDA'
+    expect(page).to have_content 'Usuário Responsável: Sergio | sergio@email.com'
+    expect(page).to have_content 'Data Prevista de Entrega: 20/12/2023'
+    expect(page).not_to have_content 'Maceio'
+    expect(page).not_to have_content 'Spark Industries Brasil LTDA'
+  end
+end
