23ad0cdc3ed0caee06309d91df1e677c6aae1502
Mateus Cavedini Pereira <mateuscavedini@gmail.com>
2023-10-26 03:37:39 -0300

    Adiciona items no estoque

--- spec/system/orders/user_update_order_status_spec.rb
+++ spec/system/orders/user_update_order_status_spec.rb
@@ -5,7 +5,9 @@ describe 'Usuário informa novo status de pedido' do
     mateus = User.create!(name: 'Mateus', email: 'mateus@email.com', password: 'password')
     warehouse = Warehouse.create!(name: 'Aeroporto SP', code: 'GRU', city: 'Guarulhos', area: 100_000, address: 'Avenida do Aeroporto, 1000', cep: '15000-000', description: 'Galpão destinado para cargas internacionais')
     supplier = Supplier.create!(corporate_name: 'ACME LTDA', brand_name: 'ACME', registration_number: '47176140000189', full_address: 'Av das Palmas, 100', city: 'Bauru', state: 'SP', email: 'contato@acme.com')
+    product = ProductModel.create!(supplier: supplier, name: 'Cadeira Games', weight: 5, height: 100, width: 70, depth: 75, sku: 'CAD-GAMER-1234')
     order = Order.create!(user: mateus, warehouse: warehouse, supplier: supplier, estimated_delivery_date: 1.day.from_now, status: :pending)
+    OrderItem.create!(order: order, product_model: product, quantity: 5)
 
     login_as mateus
     visit root_path
@@ -17,13 +19,17 @@ describe 'Usuário informa novo status de pedido' do
     expect(page).to have_content 'Status do Pedido: Entregue'
     expect(page).not_to have_button 'Marcar como ENTREGUE'
     expect(page).not_to have_button 'Marcar como CANCELADO'
+    expect(StockProduct.count).to eq 5
+    expect(StockProduct.where(product_model: product, warehouse: warehouse).count).to eq 5
   end
 
   it 'e pedido foi cancelado' do
     mateus = User.create!(name: 'Mateus', email: 'mateus@email.com', password: 'password')
     warehouse = Warehouse.create!(name: 'Aeroporto SP', code: 'GRU', city: 'Guarulhos', area: 100_000, address: 'Avenida do Aeroporto, 1000', cep: '15000-000', description: 'Galpão destinado para cargas internacionais')
     supplier = Supplier.create!(corporate_name: 'ACME LTDA', brand_name: 'ACME', registration_number: '47176140000189', full_address: 'Av das Palmas, 100', city: 'Bauru', state: 'SP', email: 'contato@acme.com')
+    product = ProductModel.create!(supplier: supplier, name: 'Cadeira Games', weight: 5, height: 100, width: 70, depth: 75, sku: 'CAD-GAMER-1234')
     order = Order.create!(user: mateus, warehouse: warehouse, supplier: supplier, estimated_delivery_date: 1.day.from_now, status: :pending)
+    OrderItem.create!(order: order, product_model: product, quantity: 5)
 
     login_as mateus
     visit root_path
@@ -35,5 +41,6 @@ describe 'Usuário informa novo status de pedido' do
     expect(page).to have_content 'Status do Pedido: Cancelado'
     expect(page).not_to have_button 'Marcar como ENTREGUE'
     expect(page).not_to have_button 'Marcar como CANCELADO'
+    expect(StockProduct.count).to eq 0
   end
 end
