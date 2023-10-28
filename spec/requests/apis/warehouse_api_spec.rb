d95444aea3d56ce0844e5de2553da3a327eda462
Mateus Cavedini Pereira <mateuscavedini@gmail.com>
2023-10-28 01:16:18 -0300

    Criação de galpões pela api

--- spec/requests/apis/warehouse_api_spec.rb
+++ spec/requests/apis/warehouse_api_spec.rb
@@ -46,5 +46,54 @@ describe 'Warehouse API' do
       json_response = JSON.parse(response.body)
       expect(json_response).to eq []
     end
+
+    it 'and raise internal error' do
+      allow(Warehouse).to receive(:all).and_raise(ActiveRecord::QueryCanceled)
+
+      get '/api/v1/warehouses'
+
+      expect(response).to have_http_status 500
+    end
+  end
+
+  context 'POST /api/v1/warehouses' do
+    it 'success' do
+      warehouse_params = { warehouse: { name: 'Aeroporto SP', city: 'Guarulhos', code: 'GRU', area: 100_000, cep: '15000-000', address: 'Avenida do Aeroporto, 1000', description: 'Galpão destinado para cargas internacionais' } }
+      
+      post '/api/v1/warehouses', params: warehouse_params
+
+      expect(response).to have_http_status 201
+      expect(response.content_type).to include 'application/json'
+      json_response = JSON.parse(response.body)
+      expect(json_response['name']).to eq 'Aeroporto SP'
+      expect(json_response['code']).to eq 'GRU'
+      expect(json_response['city']).to eq 'Guarulhos'
+      expect(json_response['area']).to eq 100_000
+      expect(json_response['cep']).to eq '15000-000'
+      expect(json_response['address']).to eq 'Avenida do Aeroporto, 1000'
+      expect(json_response['description']).to eq 'Galpão destinado para cargas internacionais'
+    end
+
+    it 'fails if parameters are incomplete' do
+      warehouse_params = { warehouse: { name: 'Aeroporto Curitiba', code: 'CWB' } }
+
+      post '/api/v1/warehouses', params: warehouse_params
+
+      expect(response).to have_http_status 412
+      expect(response.body).not_to include 'Nome não pode ficar em branco'
+      expect(response.body).not_to include 'Código não pode ficar em branco'
+      expect(response.body).to include 'Cidade não pode ficar em branco'
+      expect(response.body).to include 'Endereço não pode ficar em branco'
+    end
+
+    it 'fails if there is an internal error' do
+      allow(Warehouse).to receive(:new).and_raise(ActiveRecord::ActiveRecordError)
+
+      warehouse_params = { warehouse: { name: 'Aeroporto SP', city: 'Guarulhos', code: 'GRU', area: 100_000, cep: '15000-000', address: 'Avenida do Aeroporto, 1000', description: 'Galpão destinado para cargas internacionais' } }
+      
+      post '/api/v1/warehouses', params: warehouse_params
+
+      expect(response).to have_http_status 500
+    end
   end
 end
